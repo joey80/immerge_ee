04e506c51269110e8723b95583a79d6455b19531
Joey Leger <legerinvestments@gmail.com>
2019-01-11 10:15:38 -0500

    latest build

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
+//import './src/styles/main.scss';
 //import { uiController } from './src/scripts/components/uiController.js';
 //import { mapController } from './src/scripts/components/mapController.js';
-import './src/styles/main.scss';
-
 
 //uiController.init();
 //mapController.init();
